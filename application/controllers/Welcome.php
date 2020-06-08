5b721656e02f933e5c42a1a0163c54ccd91ed1b5
davitdaniel <bluskykind@outlook.com>
2020-06-08 09:22:49 +0800

    option_method enable

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -4,6 +4,9 @@ require APPPATH . 'libraries/REST_Controller.php';
 class Welcome extends REST_Controller {
 
 	public function __construct() {
+		header('Access-Control-Allow-Origin: *');
+   		header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+    	header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 		parent::__construct();
 		$this->load->model('api/Print_model','pm');
 	}
