c5ed03212985633bc44bc0dc2f9c308e7a603d30
davitdaniel <bluskykind@outlook.com>
2020-09-11 18:46:32 +0800

    add front-end login page

---
+++ precache-manifest.1ac39f08ac64a634fe725c9331697164.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6a0d7b8b773b03a0350af6c42b80ee1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d4227faa43cce780444",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "38697f26836601a09b5d",
+    "url": "/static/js/2.55261213.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/static/js/2.55261213.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d4227faa43cce780444",
+    "url": "/static/js/main.886420ae.chunk.js"
+  },
+  {
+    "revision": "5af87687803b6f6cd91a",
+    "url": "/static/js/runtime-main.72e5b70a.js"
+  }
+]);
