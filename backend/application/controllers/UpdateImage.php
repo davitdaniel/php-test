ec7ffcdcef6f4c958eca37632ee2c598edcabe1c
jeban-lin <abatashin@hotmail.com>
2020-10-29 04:21:52 +0800

    bug fixed updateImage.php

--- backend/application/controllers/UpdateImage.php
+++ backend/application/controllers/UpdateImage.php
@@ -8,7 +8,8 @@ class UpdateImage extends REST_Controller {
    		header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
     	header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 		parent::__construct();
-		$this->load->model('api/Print_model','pm');
+        $this->load->model('api/Print_model','pm');
+        $this->load->model('api/User_model','um');
     }
 
     public function index_get($id = 0) {
